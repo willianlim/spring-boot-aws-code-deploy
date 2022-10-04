9d18a3b65e5c3edb085703153973b7ccebd5d45e
Willian-code <willianrosendodelima@outlook.com>
2022-10-04 16:39:33 -0300

    init

---
+++ src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
@@ -0,0 +1,14 @@
+package com.sha.awscodedeploydemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("api/health")
+    public ResponseEntity<?> healtCheck() {
+        return ResponseEntity.ok("works successfully.");
+    }
+}
